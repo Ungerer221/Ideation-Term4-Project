18880ab425bdb1d48d2aa68aeaebdace3fc5d2d3
Ungerer221 <221302@virtualwindow.co.za>
2024-10-03 16:30:03 +0200

    Setting up firebase connections

---
+++ my-app/src/config/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  // ...
+  apiKey: "AIzaSyDz-jnzz4tudbQRZhVf6ppSiq2soQn5WW0",
+  authDomain: "ideation-project-a621f.firebaseapp.com",
+  projectId: "ideation-project-a621f",
+  storageBucket: "ideation-project-a621f.appspot.com",
+  messagingSenderId: "575924669758",
+  appId: "1:575924669758:web:9479b600d70eca2343306f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
